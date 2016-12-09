9e20e4245f1db56e6da06376f417cc3c0babbeb6
Seth Holladay <me@seth-holladay.com>
2016-12-09 15:46:55 -0500

    Simplify with async await.

--- index.js
+++ index.js
@@ -4,29 +4,22 @@ const path = require('path');
 const pathExists = require('path-exists');
 const nextPath = require('next-path');
 
-const findDown = (filename, option) => {
+const findDown = async (filename, option) => {
     const config = option || {};
-
     const end = path.resolve(config.cwd || '');
     let dir = path.parse(end).root;
 
-    return new Promise((resolve) => {
-        (function find() {
-            const fp = path.join(dir, filename);
-            pathExists(fp).then((exists) => {
-                if (exists) {
-                    resolve(fp);
-                }
-                else if (dir === end) {
-                    resolve(null);
-                }
-                else {
-                    dir = nextPath(dir, end);
-                    find();
-                }
-            });
-        }());
-    });
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        const fp = path.join(dir, filename);
+        if (await pathExists(fp)) {
+            return fp;
+        }
+        else if (dir === end) {
+            return null;
+        }
+        dir = nextPath(dir, end);
+    }
 };
 
 module.exports = findDown;
