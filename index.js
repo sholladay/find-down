908e07ea1fc2a8c35d0e8d294a73690f022e9fca
Seth Holladay <me@seth-holladay.com>
2017-10-26 00:19:47 -0400

    Support Node 4
    We will support Node 4 until LTS support has ended.
    
    Closes #1

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ const path = require('path');
 const locatePath = require('locate-path');
 const nextPath = require('next-path');
 
-const findDown = async (filename, option) => {
+const findDown = (filename, option) => {
     const config = option || {};
     const end = path.resolve(config.cwd || '');
     const filenames = [].concat(filename);
     let dir = path.parse(end).root;
 
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        // eslint-disable-next-line no-await-in-loop
-        const file = await locatePath(filenames, { cwd : dir });
-        if (file) {
-            return path.join(dir, file);
-        }
-        else if (dir === end) {
-            return null;
-        }
-        dir = nextPath(dir, end);
-    }
+    return new Promise((resolve) => {
+        (function find() {
+            locatePath(filenames, { cwd : dir }).then((file) => {
+                if (file) {
+                    resolve(path.join(dir, file));
+                }
+                else if (dir === end) {
+                    resolve(null);
+                }
+                else {
+                    dir = nextPath(dir, end);
+                    find();
+                }
+            });
+        }());
+    });
 };
 
 module.exports = findDown;
